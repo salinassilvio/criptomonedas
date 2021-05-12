2d9d248b11b07a635ac5bab8603e1fbda4c64950
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2021-05-11 23:42:28 -0600

    Creando nuevo componente Formulario con styled.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'; 
 import styled from '@emotion/styled';
 import imagen from './cryptomonedas.png';
+import Fomulario from './components/Formulario'
+import Formulario from './components/Formulario';
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -45,9 +47,8 @@ function App() {
         />
       </div>
       <div>
-        <Heading>
-          Cotiza Criptomonedas al Instante
-        </Heading>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario></Formulario>
       </div>
     </Contenedor>
   );
