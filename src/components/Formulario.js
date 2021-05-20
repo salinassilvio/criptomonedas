f512d1934c1b13900e6df6f1dfcfb460b035f3f2
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2021-05-20 00:58:14 -0600

    consumo de API y creacion de styled componentError

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import styled from '@emotion/styled';
 
+import Error from '../components/Error';
 import useMoneda from '../hooks/useMoneda';
+import useCriptomoneda from '../hooks/useCriptomoneda';
+import axios from 'axios';
 
 const Boton = styled.input`
     margin-top: 20px;
@@ -24,6 +27,13 @@ const Boton = styled.input`
 
 const Formulario = () => {
 
+    //state del listado de criptomonedas
+    const [listacripto,guardarCriptomonedas] = useState([]);
+
+    //error
+    const [error, guardarError] = useState(false);
+
+
     const MONEDAS = [
         {codigo: 'USD', nombre: 'Dolar de Estados Unidos'},
         {codigo: 'MXN', nombre: 'Peso Mexicano'},
@@ -35,10 +45,41 @@ const Formulario = () => {
     //utilzar useMoneda
     const [moneda,SelectMonedas] = useMoneda('Elige tu Moneda','',MONEDAS);
 
-    return ( 
-        <form>
-        <SelectMonedas></SelectMonedas>
+    //utilizar useCriptomoneda
+    const [criptoMoneda,SelectCripto] = useCriptomoneda('Elige tu Criptomoneda','',listacripto);
 
+    //Ejecutar un llamado a la API
+    useEffect(()=>{
+        const consultarAPI = async() =>{
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            
+            const resultado = await axios.get(url);
+
+            guardarCriptomonedas(resultado.data.Data);
+        }
+        consultarAPI();
+    }, []);
+
+    //cuando el usuario hace submit
+    const cotizarMoneda = e =>{
+        e.preventDefault();
+        //validar si ambos campos estan llenos
+        if(moneda === '' || criptoMoneda === ''){
+            guardarError(true);
+            return;
+        }
+
+        //pasar los datos al componente principal
+        guardarError(false);
+    }
+
+    return ( 
+        <form
+            onSubmit={cotizarMoneda}
+        >
+            {error ? <Error mensaje="Todos los campos son obligatorios"/> : null}
+        <SelectMonedas></SelectMonedas>
+        <SelectCripto></SelectCripto>
         <Boton
             type = "submit"
             value= "Calcular"
--- src/hooks/useCriptomoneda.js
+++ src/hooks/useCriptomoneda.js
@@ -22,12 +22,12 @@ const Select = styled.select`
 `;
 
 
-const useMoneda = (label,stateInicial,opciones) =>{
+const useCriptomoneda = (label,stateInicial,opciones) =>{
 
     //State de nuestro custom hook
     const [state, actualizarState] = useState(stateInicial);
 
-    const Seleccionar = () =>(
+    const SelectCripto = () =>(
         <Fragment>
             <Label>{label}</Label>
             <Select
@@ -36,16 +36,16 @@ const useMoneda = (label,stateInicial,opciones) =>{
             >
                 <option value="">-Seleccione-</option>
                 {opciones.map(opcion => (
-                    <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
+                    <option key={opcion.CoinInfo.Id} value={opcion.CoinInfo.Name}>{opcion.CoinInfo.FullName}</option>
                 ))}
             </Select>
         </Fragment>
     );
 
     //Retornar state, interfaz y fn que modifica el state
-    return [state,Seleccionar,actualizarState];
+    return [state,SelectCripto,actualizarState];
 
 }
 
-export default useMoneda;
+export default useCriptomoneda;
 
