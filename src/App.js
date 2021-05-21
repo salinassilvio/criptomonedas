20fa7c60a91154ddb5e0bd17cc094e545a75680a
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2021-05-20 23:27:38 -0600

    Creaciando un componente de cotizacion y consumir api con axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'; 
+import React, {useState,useEffect} from 'react'; 
 import styled from '@emotion/styled';
+import axios from 'axios';
 import imagen from './cryptomonedas.png';
 import Formulario from './components/Formulario';
+import Cotizacion from './components/Cotizacion';
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -37,6 +39,25 @@ const Heading = styled.h1`
 `;
 
 function App() {
+
+  const [moneda, guardarMoneda] = useState('');
+  const [criptomoneda,guardarCriptomoneda] = useState('');
+  const [resultado,guardarResultado] = useState({});
+
+  useEffect(() => {
+    const cotizarCriptomoneda = async() =>{
+          //evitamos la ejecucion la primera vez
+          if(moneda === '') return;
+
+          //consultar la api para obtener la cotizacion
+          const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+          const resultado = await axios.get(url);
+
+          guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+    }
+    cotizarCriptomoneda();
+  },[moneda,criptomoneda])
+
   return (
     <Contenedor>
       <div>
@@ -47,7 +68,13 @@ function App() {
       </div>
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
-        <Formulario></Formulario>
+        <Formulario
+          guardarMoneda={guardarMoneda}
+          guardarCriptomoneda={guardarCriptomoneda}
+        ></Formulario>
+        <Cotizacion
+          resultado={resultado}
+        ></Cotizacion>
       </div>
     </Contenedor>
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -25,7 +25,7 @@ const Boton = styled.input`
     }
 `;
 
-const Formulario = () => {
+const Formulario = ({guardarMoneda,guardarCriptomoneda}) => {
 
     //state del listado de criptomonedas
     const [listacripto,guardarCriptomonedas] = useState([]);
@@ -71,6 +71,8 @@ const Formulario = () => {
 
         //pasar los datos al componente principal
         guardarError(false);
+        guardarMoneda(moneda);
+        guardarCriptomoneda(criptoMoneda);
     }
 
     return ( 
